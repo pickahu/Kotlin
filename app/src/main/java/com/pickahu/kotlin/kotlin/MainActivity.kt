9dbe23127bc2a10fa945c32391b39f6ff85d86bc
pickahu <237202943@qq.com>
2017-06-26 10:08:49 +0800

    Kotlin环境测试通过

---
+++ app/src/main/java/com/pickahu/kotlin/kotlin/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.pickahu.kotlin.kotlin
+
+import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        message.text = "Hello Kotlin!"
+    }
+}
